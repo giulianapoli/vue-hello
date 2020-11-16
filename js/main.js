02747e9e70c9d0176a1635318331a70a767f7bef
Giulia Napoli <gnapoli1996@gmail.com>
2020-11-16 15:16:14 +0100

    Print a message using Vue data

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
+// Bonus: Aggiungere un'immagine presa anch'essa da un data.
+
+const myApp = new Vue ({
+  el: '#root',
+  data: {
+    message: 'Hello World'
+  }
+});
+// console.log(myApp);
